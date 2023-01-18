90d9a18eb4ac9693d48f3b1565b7cd60f7476092
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-18 13:43:00 -0700

    changed some routes in serverjs and used get instead of require

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 // Dependencies
 const express = require("express");
-const fs = require("fs");
 const path = require('path');
+const api = require('./routes/apiRoutes');
+const notes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
 
 // Express app
 const app = express();
-const PORT = process.env.PORT || 3001;
+
 
 // Data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', api);
 
 // Routes
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 
 
 // Listener
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
 });
